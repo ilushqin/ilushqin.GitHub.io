decc323843b59319dde606b4d06b9497c39bb48b
ilushqin <reborn703@mail.ru>
2017-03-10 15:26:27 +0300

    Add files via upload

---
+++ gallery/setup.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+	$('#pg').jphoto({
+		baseCSS: {
+			width: '175px',
+			height: '117px',
+			padding: '0px'
+		},
+		selectedCSS: {
+			top: '50px',
+			left: '300px',
+			width: '500px',
+			height: '260px',
+			padding: '10px'
+		}
+	});
+		
+});
